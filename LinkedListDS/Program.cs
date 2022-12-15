b92e4ef63bda84bbcb0d772c87bd37cfb90a7426
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-15 20:09:26 +0530

    [SaiVenkat]- Creating a Linked List

---
+++ LinkedListDS/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace LinkedListDS
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            linkedlist LinkedList = new linkedlist();
+            LinkedList.Add(56);
+            LinkedList.Add(30);
+            LinkedList.Add(70);
+        }
+    }
+}
---
+++ LinkedListDS/linkedlist.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListDS
+{
+    internal class linkedlist
+    {
+        internal node head;
+        public void Add(int id)
+        {
+            node Node = new node(id);
+            if (this.head == null)
+            {
+                this.head = Node;
+            }
+            else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                    temp.next = Node;
+                }
+
+            }
+            Console.WriteLine("{0} is inserted into the Linked List ", Node.id);
+        }
+    }
+}
